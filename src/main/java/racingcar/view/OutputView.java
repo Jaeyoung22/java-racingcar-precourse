8c640331916def39d5fbc1b06a26c79448d69e41
jaeyoung22 <yt930301@gmail.com>
2024-01-29 17:17:12 +0900

    feat: 결과와 우승자 출력하는 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,8 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.Map;
+
 public class OutputView {
 
     private static final String ERROR_PREFIX = "[ERROR]";
@@ -7,5 +10,18 @@ public class OutputView {
 
     public static void printErrorMsg(Exception e) {
         System.out.println(ERROR_PREFIX + BLANK + e.getMessage());
+    }
+
+    public static void printResultNotification() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    public static void printCurrentPositions(Map<String, Integer> currentPositions) {
+        currentPositions.forEach((key, value) -> System.out.println(key + " : " + value));
+    }
+
+    public static void printWinner(List<String> winners) {
+        System.out.println(String.join(", ", winners));
     }
 }
