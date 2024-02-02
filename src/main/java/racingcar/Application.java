a56b2335afb300fb210478744a007d567152bb98
didwldn3032 <didwldn3032@gmail.com>
2024-02-02 17:10:39 +0900

    feat: 게임 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+
+        Game game = Game.startGame(); //static이므로 바로 함수 호출 가능.
+        game.play();
     }
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -7,6 +9,32 @@ public class Car {
     public Car(String name) {
         this.name = name;
     }
-
+    public void move() {
+        if (judgeNum()) {
+            position++;
+        }
+    }
     // 추가 기능 구현
+    public int makeNum() {
+        return pickNumberInRange(0, 9);
+    }
+    public boolean judgeNum() {
+        int num = makeNum();
+        return num >= 4;
+    }
+    public void result() {
+        System.out.print(name + " : ");
+        for (int i = 0; i < position ; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
